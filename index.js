6d9d0a6b3135270a48b2eeef267613d2fd32901d
unknown <xiongjoh@gmail.com>
2020-09-17 14:30:00 -0500

    Task 2 finished

--- index.js
+++ index.js
@@ -79,8 +79,35 @@ console.log(mary.toString());
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.tank = 0;
+    this.odometer = 0;
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+  }
 
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    this.tank -= (distance/this.milesPerGallon);
+    if (this.tank < 0) {
+      this.odometer += (distance + this.tank * this.milesPerGallon)
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+    else {
+      this.odometer += distance;
+      return `I drove ${distance} miles`
+    }
+  }
 }
+
+const myCar = new Car('Camry', 30);
+console.log(myCar.tank);
+myCar.fill(10);
+console.log(myCar.tank);
+console.log(myCar.drive(100));
 
 /*
   TASK 3
