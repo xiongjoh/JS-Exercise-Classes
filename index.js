3f7d05265380767771f0e0f80b55f27824dcfac1
unknown <xiongjoh@gmail.com>
2020-09-17 14:26:27 -0500

    Task 1 finished

--- index.js
+++ index.js
@@ -41,8 +41,28 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(food) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(food);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
+
+const mary = new Person('Mary', 50);
+mary.eat('burger');
+console.log(mary.stomach);
+console.log(mary.toString());
 
 /*
   TASK 2
